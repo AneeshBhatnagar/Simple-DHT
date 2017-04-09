41091804cadc0e19491f2321bf22264e69dd9416
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-04-09 11:41:32 -0400

    Implemented Server and Client Tasks

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -1,15 +1,35 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 
-import android.content.ContentProvider;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.net.Uri;
-
 public class SimpleDhtProvider extends ContentProvider {
+    private static final int SERVER_PORT = 10000;
+    private int myPort;
+    private Context context;
+    private ServerSocket socket;
+    private boolean joinReqSent = false;
+    private boolean joinedStatus = false;
+    private final String TAG = SimpleDhtProvider.class.getSimpleName();
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -32,12 +52,26 @@ public class SimpleDhtProvider extends ContentProvider {
     @Override
     public boolean onCreate() {
         // TODO Auto-generated method stub
+        context = getContext();
+        TelephonyManager tel = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        myPort = Integer.parseInt(portStr);
+
+        try {
+            socket = new ServerSocket(SERVER_PORT);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, socket);
+
         return false;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-            String sortOrder) {
+                        String sortOrder) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -57,4 +91,76 @@ public class SimpleDhtProvider extends ContentProvider {
         }
         return formatter.toString();
     }
+
+    private class ClientTask extends AsyncTask<Object, Void, Void> {
+        @Override
+        protected Void doInBackground(Object... msg) {
+            Request request = (Request) msg[0];
+            int remotePort = (Integer) msg[1];
+            try {
+                Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                        remotePort);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(request.getJson());
+                dataOutputStream.flush();
+                Log.d("MSG SENT", request.getJson());
+                Log.d("REMOTE PORT", Integer.toString(remotePort));
+                DataInputStream dataInputStream = new DataInputStream((socket.getInputStream()));
+                socket.setSoTimeout(5000);
+                String resp = dataInputStream.readUTF();
+                if (resp.equals("OK")) {
+                    socket.close();
+                    joinReqSent = true;
+                }
+            } catch (SocketTimeoutException e) {
+                Log.d("SocketTimeOut", "Exception for" + Integer.toString(remotePort));
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d("IOEXCEPTION", "Timeout on " + Integer.toString(remotePort));
+                e.printStackTrace();
+                joinReqSent = false;
+            }
+
+            return null;
+        }
+    }
+
+    private class ServerTask extends AsyncTask<ServerSocket, Request[], Void> {
+
+        ServerSocket serverSocket;
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            serverSocket = sockets[0];
+            while (true) {
+                try {
+                    Log.d(TAG,"Server Task Started");
+                    Socket socket = serverSocket.accept();
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                    String jsonString = dataInputStream.readUTF();
+                    Request request = new Request(jsonString);
+                    dataOutputStream.writeUTF("OK");
+                    socket.close();
+                    dataOutputStream.flush();
+                    dataInputStream.close();
+                    dataOutputStream.close();
+                    Log.d("MSG RECEIVED", jsonString);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+
+            return null;
+        }
+
+        protected void onProgressUpdate(Request[]... messages) {
+            return;
+        }
+
+    }
+
+
 }
