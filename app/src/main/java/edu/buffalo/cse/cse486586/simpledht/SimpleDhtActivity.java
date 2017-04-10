8776a5cd236529e58f603a3e6c682a7ad2cd2335
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-04-10 12:23:11 -0400

    Implemented Debugging tools for Local and Global Dump

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
@@ -1,22 +1,64 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SimpleDhtActivity extends Activity {
-
+    private TextView tv;
+    private ContentResolver contentResolver;
+    private Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_dht_main);
+        Button globalTest, localTest;
+        contentResolver = getContentResolver();
+        globalTest = (Button) findViewById(R.id.button2);
+        localTest = (Button) findViewById(R.id.button1);
+
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.authority("edu.buffalo.cse.cse486586.simpledht.provider");
+        uriBuilder.scheme("content");
+        uri = uriBuilder.build();
+
+        globalTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor resultCursor = contentResolver.query(uri, null,
+                        "*", null, null);
+                displayCursorOnTextView(resultCursor);
+            }
+        });
+
+        localTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor resultCursor = contentResolver.query(uri, null,
+                        "@", null, null);
+                displayCursorOnTextView(resultCursor);
+            }
+        });
         
-        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
         findViewById(R.id.button3).setOnClickListener(
                 new OnTestClickListener(tv, getContentResolver()));
+    }
+
+    public void displayCursorOnTextView(Cursor cursor){
+        cursor.moveToFirst();
+        while(cursor.moveToNext()){
+            tv.append(cursor.getString(0) + ":" + cursor.getString(1) + "\n");
+        }
     }
 
     @Override
